d422acb4aee04efa51f79e9444354b78ec86fb47
codefatherru <vasiliy.m.leonov@gmail.com>
2023-02-28 19:27:40 +0300

    удалось запустить

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Роутеры. Структура
+# Глава 6: Особые апдейты my_chat_member и chat_member 
 
-В этом каталоге исходники к главе https://mastergroosha.github.io/aiogram-3-guide/routers/
+В этом каталоге исходники к главе https://groosha.2038.io/aiogram-3-guide/special-updates
+
+
+добавлен env MAIN_CHAT_ID
+
+#обшибки
+не обработано aiogram.exceptions.TelegramBadRequest: Bad Request: there are no administrators in the private chat
--- bot.py
+++ bot.py
@@ -4,25 +4,32 @@ import os
 from aiogram import Bot, Dispatcher
 
 #from config_reader import config
-from handlers import group_games, checkin, usernames, questions, different_types
+from handlers import in_pm, bot_in_group, admin_changes_in_group, events_in_group, group_games, checkin, usernames, questions, different_types
 from middlewares.weekend import WeekendCallbackMiddleware
-
 
 async def main():
     bot = Bot(token=os.getenv('SIM_BOT_TOKEN'))
     dp = Dispatcher()
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
 
     dp.include_router(group_games.router)
     dp.include_router(checkin.router)
     dp.include_router(usernames.router)
+    dp.include_router(in_pm.router)
+    dp.include_router(events_in_group.router)
+    dp.include_router(bot_in_group.router)
+    dp.include_router(admin_changes_in_group.router)
 
     dp.callback_query.outer_middleware(WeekendCallbackMiddleware())
 
-    # Запускаем бота и пропускаем все накопленные входящие
-    # Да, этот метод можно вызвать даже если у вас поллинг
+    admins = await bot.get_chat_administrators(os.getenv('MAIN_CHAT_ID'))
+    admin_ids = {admin.user.id for admin in admins}
+
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types(), admins=admin_ids)
 
 
 if __name__ == "__main__":
